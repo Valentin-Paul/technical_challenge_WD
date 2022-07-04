c3e76b4b84f76e927c8f9061d9949958f01997f7
Valentin Paul <valentin@vamptech.de>
2022-07-04 11:37:54 +0200

    created connection with database and created routes

---
+++ app.js
@@ -0,0 +1,27 @@
+const express = require('express')
+
+const mongoose = require('mongoose')
+const Phones = require('./models/Phones.Model')
+
+const app = express();
+app.listen(3000, () => console.log("Server is running"));
+
+mongoose
+  .connect('mongodb://localhost:27017/technical-challenge')
+  .then(x => console.log(`Connected to Mongo! Database name: "${x.connections[0].name}"`))
+  .catch(err => console.error('Error connecting to mongo', err))
+
+
+app.get("/phones", (req, res, next)=>{
+    Phones.find()
+    .then(response=> res.json(response))
+    .catch(err=>console.log('error getting phones', err))
+})
+
+app.get("/phones/:phoneId", (req,res,next)=>{
+    Phones.findById(req.params.phoneId)
+    .then(response=>res.json(response))
+    .catch(err=>console.log('error getting phone details', err))
+})
+
+module.exports = app;
